6e4b6dd7b8af2e0c3e42ece81df61150a6c21d0e
Donald Johnson <copyleftdev@gmail.com>
2016-10-28 00:26:16 -0700

    Added a series of status 200 tests

--- petco_smoke.py
+++ petco_smoke.py
@@ -1,6 +1,6 @@
 import unittest
 import requests
-
+from bs4 import BeautifulSoup as bs
 
 
 class PetcoSmoke(unittest.TestCase):
@@ -19,7 +19,7 @@ class PetcoSmoke(unittest.TestCase):
     def test_petco_local_is_us(self):
         """"Very Basic test to assert site is set to en-US"""
         r = requests.get('http://www.petco.com')
-        self.assertEqual(r.headers['Content-Language'],"en-US")
+        self.assertEqual(r.headers['Content-Language'], "en-US")
 
     def test_petco_cookie_is_issued(self):
         r = requests.get('http://www.petco.com')
@@ -27,9 +27,106 @@ class PetcoSmoke(unittest.TestCase):
 
     def test_petco_content_Encoding(self):
         r = requests.get('http://www.petco.com')
-        self.assertEqual(r.headers['Content-Encoding'],'gzip')
+        self.assertEqual(r.headers['Content-Encoding'], 'gzip')
 
+    def test_petco_header_widget(self):
+        r = requests.get('http://www.petco.com')
+        s = bs(r.text, 'html.parser')
+        result = s.find(id='header')
 
+        self.assertGreaterEqual(result.get_text(), 0)
+
+    def test_petco_dog_training(self):
+        r = requests.get("http://www.petco.com/dog-training")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_events(self):
+        r = requests.get("http://www.petco.com/events")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_gitcards(self):
+        r = requests.get("http://www.petco.com/gift-cards")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_grooming(self):
+        r = requests.get("http://www.petco.com/grooming")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_help(self):
+        r = requests.get("http://www.petco.com/help")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_kids(self):
+        r = requests.get("http://www.petco.com/kids")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_pet_center(self):
+        r = requests.get("http://www.petco.com/new-pet-center")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_newpet(self):
+        r = requests.get("http://www.petco.com/newpet")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_gitcards(self):
+        r = requests.get("http://www.petco.com/gift-cards")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_gitcards(self):
+        r = requests.get("http://www.petco.com/gift-cards")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_nutrition(self):
+        r = requests.get("http://www.petco.com/nutrition")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_order_status(self):
+        r = requests.get("http://www.petco.com/order-status")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_pals_reward(self):
+        r = requests.get("http://www.petco.com/pals-rewards")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_pet_care(self):
+        r = requests.get("http://www.petco.com/pet-care-resource-center")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_pet_service(self):
+        r = requests.get("http://www.petco.com/pet-services")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_promo_coupons(self):
+        r = requests.get("http://www.petco.com/petco-coupons-promos")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_foundation(self):
+        r = requests.get("http://www.petco.com/petco-foundation")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_pharmacy(self):
+        r = requests.get("http://www.petco.com/pharmacy")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_private_policy(self):
+        r = requests.get("http://www.petco.com/privacy-policy")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_product_guides(self):
+        r = requests.get("http://www.petco.com/product-guides")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_repeat_delivery(self):
+        r = requests.get("http://www.petco.com/repeat-delivery")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_returns(self):
+        r = requests.get("http://www.petco.com/returns")
+        self.assertEqual(r.status_code, 200)
+
+    def test_petco_shipping(self):
+        r = requests.get("http://www.petco.com/shipping")
+        self.assertEqual(r.status_code, 200)
 
 
 if __name__ == '__main__':
